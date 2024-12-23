04dfb188cf3d953809074f3d6e2560bd9d9e7eb5
Yash Jangid <yashcx84@gmail.com>
2024-12-23 15:43:12 +0530

    verification on the way

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Change to the project directory
-cd /home/u525176569/domains/taxirentaljaipur.com/Jaipur-Taxi-Service
-
-# Ensure the latest changes are pulled from GitHub
-git pull origin master
-
